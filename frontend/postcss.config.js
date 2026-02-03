d3483d8558a60c1ff8bb44e8f22b1cca4e0d7ab7
MR.DarkPromth Admin <admin@mr-darkpromth.com>
2026-02-04 02:41:34 +0700

    Update frontend Docker and styling

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,10 @@
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.js';
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwind(tailwindConfig),
+    autoprefixer,
+  ],
+};
